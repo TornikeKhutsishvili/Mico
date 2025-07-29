08d24895234c1357cbbc2345d25105690ea5ab86
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2025-07-29 13:12:08 +0400

    Add treatment_a css and js files, also modify treatment-section, treatment, index, data php files

--- treatment.php
+++ treatment.php
@@ -14,38 +14,19 @@
 
   <title>Mico</title>
 
+  <link rel="stylesheet" href="css/treatment_a.css">
   <!-- Core CSS -->
   <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-
   <!-- Google Fonts -->
-  <link
-    href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
-    rel="stylesheet"
-  />
-
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
   <!-- Owl Carousel CSS -->
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-  />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
   <!-- Font Awesome -->
   <link rel="stylesheet" href="css/font-awesome.min.css" />
-
   <!-- jQuery Nice Select CSS -->
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css"
-    integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4="
-    crossorigin="anonymous"
-  />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
   <!-- Bootstrap Datepicker CSS -->
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css"
-  />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css" />
   <!-- Custom Styles -->
   <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="css/responsive.css" />
@@ -54,31 +35,21 @@
 <body class="sub_page">
 
   <!-- Header Section -->
-  <?php 
-    include("./sections/header.php"); 
-    set_header($contact, $navbarItems); 
-  ?>
+  <?php include("./sections/header.php"); set_header($contact, $navbarItems); ?>
 
   <!-- Treatment Section -->
-  <?php 
-    include("./sections/treatment-section.php"); 
-    treatment_section($hospital_treatments_cards); 
-  ?>
+  <?php include("./sections/treatment-section.php"); treatment_section($hospital_treatments_cards); ?>
 
   <!-- Info Section -->
-  <?php 
-    include("./sections/info-section.php"); 
-    info($infoData); 
-  ?>
+  <?php include("./sections/info-section.php"); info($infoData); ?>
 
   <!-- Footer Section -->
-  <?php 
-    include("./sections/footer.php"); 
-    set_footer(); 
-  ?>
+  <?php include("./sections/footer.php"); set_footer(); ?>
+
 
   <!-- Scripts -->
   <script src="js/button.js"></script>
+  <script src="js/read-more-less.js"></script>
   <!-- jQery -->
   <script src="js/jquery-3.4.1.min.js"></script>
   <!-- bootstrap js -->
